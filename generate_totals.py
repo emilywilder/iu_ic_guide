ecc421fc4627c5e5477735bbdebe29b0bee1d6f4
Emily Wilder <emilywilder.earth@gmail.com>
2013-01-27 02:35:32 -0500

    add ability to provide obtained items
    can pass --obtaineditems argument to give a file in the same format as
    neededitems. this will deduct requirements for items you already have.

--- generate_totals.py
+++ generate_totals.py
@@ -5,12 +5,14 @@ import logging
 ITEMS_DB = "iuitems.json"
 
 class GenerateItems:
-    def __init__(self, item_db_file, needed_items_file):
+    def __init__(self, item_db_file, needed_items_file, obtained_items_file=None):
         self.logger = logging.getLogger("GenerateItems")
         self.item_db_file = item_db_file
         self.needed_items_file = needed_items_file
+        self.obtained_items_file = obtained_items_file
         self.items_db = {}
         self.needed_items = []
+        self.obtained_items = {}
         self.materials = {}
 
     def _loaditemsdb(self):
@@ -24,9 +26,18 @@ class GenerateItems:
                 (_num, _obj) = _line.strip().split('|')
                 self.needed_items.append({"num": int(_num), "obj": _obj})
 
+    def _loadobtaineditems(self):
+        self.obtained_items = {}
+        if self.obtained_items_file:
+            with open(self.obtained_items_file, "r") as f:
+                for _line in f:
+                    (_num, _obj) = _line.strip().split('|')
+                    self.obtained_items[_obj] = int(_num)
+
     def _loaddata(self):
         self._loaditemsdb()
         self._loadneededitems()
+        self._loadobtaineditems()
 
     def aggregate(self, recursive=False):
         self._loaddata()
@@ -46,15 +57,20 @@ class GenerateItems:
             self.materials[item.get("obj")] = int(item.get("num"))
 
     def _aggregate(self, dataset, recursive=False):
-        self.logger.debug("DEBUG: _aggregate called")
         for dep in dataset:
-            self.logger.debug("DEBUG: dep <{0}>".format(dep))
+            left = self.obtained_items.get(dep.get("obj"), 0) - int(dep.get("num"))
+            self.obtained_items[dep.get("obj")] = max(0, left)
+            if left < 0:
+                dep = {"num": abs(left), "obj": dep.get("obj")}
+            else:
+                dep = {"num": 0, "obj": dep.get("obj")}
             if recursive and self.items_db.has_key(dep.get("obj")):
                 rec = self.items_db.get(dep.get("obj")).get("ic")
                 for i in xrange(int(dep.get("num"))):
                     self._aggregate(rec, recursive)
             else:
-                self._storematerial(dep)
+                if dep.get("num") > 0:
+                    self._storematerial(dep)
 
     def report(self):
         for k, v in sorted(self.materials.items()):
@@ -66,6 +82,7 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true", default=False)
     parser.add_argument("--itemdb", default=ITEMS_DB,
                         help="file of IC items in JSON format")
+    parser.add_argument("--obtaineditems", help="file of items already obtained")
     parser.add_argument("--recursive", action="store_true", default=False,
                         help="aggregate totals recursively")
     parser.add_argument("neededitems", help="file of items needed")
@@ -79,7 +96,7 @@ if __name__ == "__main__":
 
     logging.basicConfig(format="%(message)s", level=llevel)
 
-    gi = GenerateItems(args.itemdb, args.neededitems)
+    gi = GenerateItems(args.itemdb, args.neededitems, args.obtaineditems)
     gi.aggregate(recursive=args.recursive)
     gi.report()
 
